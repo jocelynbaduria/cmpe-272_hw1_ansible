6651b70e42a7ee6893bf1a5a9c456f4cada32e71
Jocelyn Baduria <jocelynbbaduria@gmail.com>
2020-09-01 22:35:17 +0000

    Modified installansible.sh

--- installansible.sh
+++ installansible.sh
@@ -3,9 +3,3 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install ansible
-
-#deploy apache webserver
-ansible-playbook deploy_apache.yml
-
-#undeploy apache webserver
-ansible-playbook undeploy_apache.yml
